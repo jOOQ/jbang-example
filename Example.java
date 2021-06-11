ade9f111dfe91087c58a3a91fd6a55a890fbfebc
Lukas Eder <lukas.eder@gmail.com>
2021-06-11 15:38:26 +0200

    [jOOQ/jOOQ#11492] Add jbang example to the manual

---
+++ Example.java
@@ -0,0 +1,45 @@
+//DEPS org.jooq:jooq:${jooq.version:RELEASE}
+//DEPS com.h2database:h2:${h2.version:RELEASE}
+//SOURCES com/example/db/**
+
+import static org.jooq.impl.DSL.*;
+import static com.example.db.Tables.*;
+import org.jooq.*;
+import java.io.*;
+import java.util.*;
+import com.example.db.tables.records.*;
+
+
+public class Example {
+	public static void main(String[] args) throws Exception {
+		try (CloseableDSLContext ctx = using("jdbc:h2:mem:jooq-jbang-example")) {
+			execute(ctx, "db.sql");
+			execute(ctx, "data.sql");
+       
+			var query =
+				ctx.select(
+					BOOK_TO_BOOK_STORE.book().author().FIRST_NAME,
+					BOOK_TO_BOOK_STORE.book().author().LAST_NAME,
+					BOOK_TO_BOOK_STORE.book().TITLE,
+					countDistinct(BOOK_TO_BOOK_STORE.BOOK_ID)
+						.over(partitionBy(BOOK_TO_BOOK_STORE.book().AUTHOR_ID)).as("books written by author"),
+					countDistinct(BOOK_TO_BOOK_STORE.NAME)
+						.over(partitionBy(BOOK_TO_BOOK_STORE.book().AUTHOR_ID)).as("bookstores listing author"),
+					BOOK_TO_BOOK_STORE.book().language().CD,
+					BOOK_TO_BOOK_STORE.bookStore().NAME)
+				   .from(BOOK_TO_BOOK_STORE)
+				   .orderBy(1, 2, 3);
+
+			System.out.println("Query:");
+			System.out.println(query);
+
+			System.out.println();
+			System.out.println("Result:");
+			System.out.println(query.fetch());
+		}
+	}
+
+	private static void execute(DSLContext ctx, String file) throws Exception {
+		ctx.parser().parse(Source.of(new File(file)).readString()).executeBatch();
+	}
+}
---
+++ data.sql
@@ -0,0 +1,30 @@
+-- See https://www.jooq.org/doc/latest/manual/getting-started/sample-database/
+INSERT INTO language (id, cd, description) VALUES (1, 'en', 'English');
+INSERT INTO language (id, cd, description) VALUES (2, 'de', 'Deutsch');
+INSERT INTO language (id, cd, description) VALUES (3, 'fr', 'Français');
+INSERT INTO language (id, cd, description) VALUES (4, 'pt', 'Português');
+
+INSERT INTO author (id, first_name, last_name, date_of_birth    , year_of_birth)
+  VALUES           (1 , 'George'  , 'Orwell' , DATE '1903-06-26', 1903         );
+INSERT INTO author (id, first_name, last_name, date_of_birth    , year_of_birth)
+  VALUES           (2 , 'Paulo'   , 'Coelho' , DATE '1947-08-24', 1947         );
+
+INSERT INTO book (id, author_id, title         , published_in, language_id)
+  VALUES         (1 , 1        , '1984'        , 1948        , 1          );
+INSERT INTO book (id, author_id, title         , published_in, language_id)
+  VALUES         (2 , 1        , 'Animal Farm' , 1945        , 1          );
+INSERT INTO book (id, author_id, title         , published_in, language_id)
+  VALUES         (3 , 2        , 'O Alquimista', 1988        , 4          );
+INSERT INTO book (id, author_id, title         , published_in, language_id)
+  VALUES         (4 , 2        , 'Brida'       , 1990        , 2          );
+
+INSERT INTO book_store VALUES ('Orell Füssli');
+INSERT INTO book_store VALUES ('Ex Libris');
+INSERT INTO book_store VALUES ('Buchhandlung im Volkshaus');
+
+INSERT INTO book_to_book_store VALUES ('Orell Füssli'             , 1, 10);
+INSERT INTO book_to_book_store VALUES ('Orell Füssli'             , 2, 10);
+INSERT INTO book_to_book_store VALUES ('Orell Füssli'             , 3, 10);
+INSERT INTO book_to_book_store VALUES ('Ex Libris'                , 1, 1 );
+INSERT INTO book_to_book_store VALUES ('Ex Libris'                , 3, 2 );
+INSERT INTO book_to_book_store VALUES ('Buchhandlung im Volkshaus', 3, 1 );
